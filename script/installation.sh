0eda7691b894c6ab616e27f98f93b949770e65de
eman602 <64255340+eman602@users.noreply.github.com>
2022-02-03 00:24:29 +0000

    Update installation.sh

--- script/installation.sh
+++ script/installation.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
  
  
-sudo cp etc/systemd/system/flask.service /etc/systemd/system/
+cp etc/systemd/system/flask.service /etc/systemd/system/
 
-sudo systemctl daemon-reload
+systemctl daemon-reload
 
-sudo systemctl enable flask.service
+systemctl enable flask.service
 
 source venv/bin/activate
 
